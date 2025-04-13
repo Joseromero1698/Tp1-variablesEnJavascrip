076c942830bac341802f37bc64f9a0dfe40f2f88
Jose Romero <kira1698@gmail.com>
2025-04-13 17:26:52 -0300

    se agregaron mas archivos en la carpeta js necesarios para cumplir con una de las consignas del trabajo

--- js/1-variables.js
+++ js/1-variables.js
@@ -18,3 +18,27 @@ document.writeln ("El resultado de la suma es" + resultado)
  */
 const nombre= prompt("Ingrese su nombre")
 document.writeln("<br>Hola" + nombre)
+/* 5.- Escribe un programa de tres líneas que pida un número, pida otro número y escriba el resultado de sumar estos dos números.
+Nota: Tener en cuenta la siguiente función: parseInt
+ */
+
+/* 6.- Escribe un programa que pida dos números y escriba en la pantalla cual es el mayor.
+ */
+
+/* 7.- Escribe un programa que pida 3 números y escriba en la pantalla el mayor de los tres.
+Ejemplo: 
+ */
+
+/* 8.- Escribe un programa que pida un número y diga si es divisible por 2
+Ejemplo: 
+ */
+
+/*  9.- Escribe un programa que pida una frase y escriba las vocales que aparecen
+Nota: Tener en cuenta la función length y substring o charAt (developer mozilla)
+ */
+
+/* 10.- Escribe un programa que pida un número y nos diga si es divisible por 2, 3, 5 o 7 (sólo hay que comprobar si lo es por uno de los cuatro)
+ */
+
+/* 11.- Añadir al ejercicio anterior que nos diga por cuál de los cuatro es divisible (hay que decir todos por los que es divisible)
+ */
