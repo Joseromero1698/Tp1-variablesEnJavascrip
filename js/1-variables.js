3286610bc665f4cc1760f4bf977a590f68eab6c9
Jose Romero <kira1698@gmail.com>
2025-04-13 12:03:33 -0300

    se realizan los ejercicios del punto 1 al 4

---
+++ js/1-variables.js
@@ -0,0 +1,20 @@
+console.log("mesnaje de prueba");
+
+/* 1.- Escribe un programa de una sola línea que haga que aparezca en la pantalla un alert que diga “un mensaje”.
+ */
+ alert ("un mensaje")
+
+ /* 2.- Escribe un programa de una sola línea que escriba en la pantalla un texto que diga «Hello World» (document.write). */
+ document.writeln ("Hello World <br>")
+
+ /* 3.- Escribe un programa de una sola línea que escriba en la pantalla el resultado de sumar 3 + 5.
+ */
+const numero1= 3
+const numero2= 5
+const resultado= numero1 + numero2
+document.writeln ("El resultado de la suma es" + resultado)
+
+/* 4.- Escribe un programa de dos líneas que pida el nombre del usuario con un prompt y escriba un texto que diga «Hola nombreUsuario»
+ */
+const nombre= prompt("Ingrese su nombre")
+document.writeln("<br>Hola" + nombre)
